fb0f840982afbe798802f0993046d2894384e1b5
wax <Julian04121950@gmail.com>
2025-11-21 11:33:36 -0500

    No login (Pruebas)

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,3 @@
-import { StripeProvider } from '@stripe/stripe-react-native';
 import { Stack } from 'expo-router';
 import { AuthProvider } from '../Context/AuthContext';
 import { ExerciseProvider } from '../Context/ExerciseContext';
@@ -6,18 +5,18 @@ import { ExerciseProvider } from '../Context/ExerciseContext';
 export default function RootLayout() {
     return (
         <AuthProvider>
-            <StripeProvider publishableKey="pk_test_51SSSpX34nB0iTmwzgNwrvD2aR0rtC7b8Wm2GMwPtgVkN6A3fwUcqfZHccXt7780CfH5UGKRhvTHLTFzKOGJiZCZA000cLum5VS">
+
             <ExerciseProvider>
-            <Stack
-                initialRouteName='index'
-                screenOptions={{ headerShown: false }}
-            >
-                <Stack.Screen name="index" />
-                <Stack.Screen name="(auth)" />
-                <Stack.Screen name="(main)" />
-            </Stack>
+                <Stack
+                    initialRouteName='index'
+                    screenOptions={{ headerShown: false }}
+                >
+                    <Stack.Screen name="index" />
+                    <Stack.Screen name="(auth)" />
+                    <Stack.Screen name="(main)" />
+                </Stack>
             </ExerciseProvider>
-            </StripeProvider>
+
         </AuthProvider>
     );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -3,5 +3,5 @@ import { Redirect } from "expo-router";
 
 export default function Index() {
   // Redirige automáticamente al grupo auth/login
-  return <Redirect href="/(auth)/login" />;
+  return <Redirect href="/(main)/exercises" />;
 }
